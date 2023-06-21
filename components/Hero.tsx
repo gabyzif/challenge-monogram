6e400449e24949c10f34e81235857736e1b3d4db
gabyzif <gabyzif@gmail.com>
2023-06-21 19:45:28 -0300

    adding hero and images

---
+++ components/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        <h1>
+          jamstack <span>101</span>
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu augue massa, tincidunt proin nunc
+          viverra tristique tempor, ipsum. At lectus vel pretium tempor ut dui vivamus sit.
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
