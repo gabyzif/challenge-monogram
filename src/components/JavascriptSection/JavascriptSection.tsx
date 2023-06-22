e66ea5f41a76ea8cfd47bc558116c6badf0805e0
gabyzif <gabyzif@gmail.com>
2023-06-21 22:48:47 -0300

    center stuff

--- src/components/JavascriptSection/JavascriptSection.tsx
+++ src/components/JavascriptSection/JavascriptSection.tsx
@@ -2,7 +2,7 @@ import Computer from './Computer';
 import './JavascriptSection.css';
 const JavascriptSection = () => {
   return (
-    <div className="overflow-visible px-[3.25rem] ">
+    <div className="m-auto px-[3.25rem] ">
       <div className="sectionBackground py-10">
         <div className="m-auto mb-20 w-2/4">
           <h2 className="title mb-5 text-white">javascript</h2>
