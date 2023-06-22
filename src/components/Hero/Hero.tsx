f2750ec257bb1cc2d2c253119e267231f5d9c3ce
gabyzif <gabyzif@gmail.com>
2023-06-21 21:10:23 -0300

    hero finish

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,19 +1,23 @@
+/* eslint-disable @next/next/no-img-element */
 import HeroGraphics from './HeroGraphics';
 import './Hero.css';
 const Hero = () => {
   return (
-    <div className="flex px-[7.25rem]">
-      <div>
-        <h1 className="title mb-10">
-          jamstack <span className="textWithImage">101</span>
-        </h1>
-        <p className="subtitle ml-[3.87rem]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu augue massa, tincidunt proin nunc
-          viverra tristique tempor, ipsum. At lectus vel pretium tempor ut dui vivamus sit.
-        </p>
-      </div>
-      <div className="relative ">
-        <HeroGraphics />
+    <div className="px-[7.25rem]">
+      <h1 className="title mb-10">
+        jamstack <span className="textWithImage">101</span>
+      </h1>
+      <div className="flex ">
+        <div>
+          <p className="subtitle mb-[100px] ml-[3.87rem]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu augue massa, tincidunt proin nunc
+            viverra tristique tempor, ipsum. At lectus vel pretium tempor ut dui vivamus sit.
+          </p>
+          <img className="m-auto" src="/images/mouse-line.svg" alt="Mouse Line" />
+        </div>
+        <div className="mt-[-100px] ">
+          <HeroGraphics />
+        </div>
       </div>
     </div>
   );
